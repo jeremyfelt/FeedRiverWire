1a3212cce5b1d23c11ff96a32ee2cd8ab732f002
Jeremy Felt <jeremy.felt@gmail.com>
2011-11-15 23:47:18 -0800

    Remove automatic source changing entirely for now.

--- index.php
+++ index.php
@@ -7,13 +7,6 @@ require_once( dirname( __FILE__ ) . '/includes/functions.php' );
 session_start();
 $_SESSION[ 'last_item_id' ] = NULL;
 
-/*  This is a hacky way to determine what sources we're looking at. */
-if ( ! $_SESSION[ 'river_sources' ] ) {
-    $_SESSION[ 'river_sources' ] = '1,2,3';
-}
-
-$river_source_ids = $_SESSION[ 'river_sources' ];
-
 $recent_river_items = get_recent_river_items( $river_source_ids, 1, 20 );
 
 $last_item_id = 1;
