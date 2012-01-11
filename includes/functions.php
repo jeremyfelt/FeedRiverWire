e498c13b2f881c8cb3dc2291b195dcbd0f676758
Jeremy Felt <jeremy.felt@gmail.com>
2012-01-10 19:49:19 -0800

    Create some functions, put them in a file.

--- includes/functions.php
+++ includes/functions.php
@@ -44,3 +44,92 @@ function get_display_class( $river_source_id ){
     }
     return $extra_class;
 }
+
+function add_hn_items( $hn_data ){
+
+    $db = db_connect();
+
+    $decoded_data = json_decode( $hn_data );
+    $capture_date = date( 'Y-m-d H:i:s' );
+
+    foreach ( $decoded_data->items as $item ) {
+        if ( "http" != substr( $item->url, 0, 4 ) ){
+            $this_item_url = "http://news.ycombinator.com" . $item->url;
+            $this_item_source = "Ask HN";
+            $this_item_publication = "Hacker News";
+        }else{
+            $this_item_url = $item->url;
+            $this_item_source = "New";
+            $this_item_publication = "Hacker News";
+        }
+        $this_discussion_url = "http://news.ycombinator.com/item?id=" . $item->id;
+        $this_item_id = $feed_unique_prefix . '_' . $item->id;
+
+        $item_update_query = $db->prepare( "INSERT INTO river_items ( river_source_id, feed_item_id, item_url,
+            item_title, item_author, permalink, publish_date, feed_section, feed_title, capture_date)
+            VALUES
+            ( :river_source_id, :feed_item_id, :item_url, :item_title, :item_author, :permalink,
+            :publish_date, :feed_section, :feed_title, :capture_date)
+            ON DUPLICATE KEY UPDATE capture_date = :capture_date" );
+        $item_update_query->bindValue( ':river_source_id', 2 );
+        $item_update_query->bindParam( ':feed_item_id', $this_item_id );
+        $item_update_query->bindParam( ':item_url', $this_item_url );
+        $item_update_query->bindParam( ':item_title', $item->title );
+        $item_update_query->bindParam( ':item_author', $item->postedBy );
+        $item_update_query->bindParam( ':permalink', $this_discussion_url );
+        $item_update_query->bindParam( ':publish_date', date( 'Y-m-d H:i:s', strtotime( $item->postedAgo ) ) );
+        $item_update_query->bindParam( ':feed_section', $this_item_source );
+        $item_update_query->bindParam( ':feed_title', $this_item_publication );
+        $item_update_query->bindParam( ':capture_date', $capture_date );
+        $item_update_query->execute();
+        $item_update_query = NULL;
+    }
+
+    $db = NULL;
+
+}
+
+function add_nyt_items( $nyt_data ){
+
+    $db = db_connect();
+
+    $decoded_data = json_decode( $nyt_data );
+    $capture_date = date( 'Y-m-d H:i:s' );
+
+    foreach ( $decoded_data->results as $item ){
+        $this_item_id = $feed_unique_prefix . '_' . md5( $item->url );
+        if ( "by" == strtolower( substr( $item->byline, 0, 2 ) ) ){
+            $this_item_author = trim( substr( $item->byline, 2 ) );
+        }else{
+            $this_item_author = $item->byline;
+        }
+
+        if ( "Blog" == $item->item_type ){
+            $this_feed_title = "NY Times - " . $item->blog_name;
+        }else{
+            $this_feed_title = $item->source;
+        }
+
+        $item_update_query = $db->prepare( "INSERT INTO river_items ( river_source_id, feed_item_id, item_url,
+            item_title, item_author, publish_date, body, feed_section, feed_title, capture_date)
+            VALUES
+            ( :river_source_id, :feed_item_id, :item_url, :item_title, :item_author, :publish_date,
+              :body, :feed_section, :feed_title, :capture_date )
+            ON DUPLICATE KEY UPDATE capture_date = :capture_date" );
+        $item_update_query->bindValue( ':river_source_id', 3 );
+        $item_update_query->bindParam( ':feed_item_id', $this_item_id );
+        $item_update_query->bindParam( ':item_url', $item->url );
+        $item_update_query->bindParam( ':item_title', $item->title );
+        $item_update_query->bindParam( ':item_author', $this_item_author );
+        $item_update_query->bindParam( ':publish_date', date( 'Y-m-d H:i:s', strtotime( $item->updated_date ) ) );
+        $item_update_query->bindParam( ':body', $item->abstract );
+        $item_update_query->bindParam( ':feed_section', $item->section );
+        $item_update_query->bindParam( ':feed_title', $this_feed_title );
+        $item_update_query->bindParam( ':capture_date', $capture_date );
+        $item_update_query->execute();
+        $item_update_query = NULL;
+    }
+
+    $db = NULL;
+
+}
